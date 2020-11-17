5f993959547d4eec6ca18be03d88a6204ff7d80f
Aadi Kuchlous <aadikuchlous@gmail.com>
2020-11-17 23:30:31 +0530

    added hindi data/changed gameplay (hidden options)/added long clips after round is over

--- eng_data.js
+++ eng_data.js
@@ -1,53 +1,13 @@
-var eng_data = 
+var eng_data =
 [
-	{
-		"audio": ["Song_Clips_Eng/attention.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Shake it off - Taylor Swift", "Attention - Charlie Puth", "New Rules - Dua Lipa", "Attention - Shawn Mendes"],
-		"correct_answer": 2
-	},
-	{
-		"audio": ["Song_Clips_Eng/back_to_you.mp3", "Song_Clips_Eng/attention.mp3", "Song_Clips_Eng/believer.mp3"],
-		"answers": ["Back to you - Salena Gomez", "Wolves - Salena Gomez", "Look what you made me do - Taylor swift", "Break free - Ariana Grande"],
-		"correct_answer": 1
-	},
-	{
-		"audio": ["Song_Clips_Eng/believer.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Stressed out - Twenty One Pilots", "Mercy - One republic", "Believer - Imagine dragons", "If I lose Myself - One Republic"],
-		"correct_answer": 3
-	},
-	{
-		"audio": ["Song_Clips_Eng/another_brick_in_the_wall.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Break On Through - The Doors", "Another brick in the wall - Pink Floyd", "On an island - David Gilmour", "No Quarter - Led Zeppelin"],
-		"correct_answer": 2
-	},
-	{
-		"audio": ["Song_Clips_Eng/another_one_bites_the_dust.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Under Pressure - Queen", "Smoke on the Water - Deep Purple", "Livin' On A Prayer - Bon Jovi", "Another one bites the dust - Queen"],
-		"correct_answer": 4
-	},
-	{
-		"audio": ["Song_Clips_Eng/blinding_lights.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Yummy - Justin Beiber", "Blinding lights - The Weeknd", "Up all Night - Khalid", "Something In the Way You Move - Ellie Goulding"],
-		"correct_answer": 2
-	},
-	{
-		"audio": ["Song_Clips_Eng/chandelier.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Chandelier - Sia", "Elastic Heart - Sia", "Blank Space - Taylor Swift", "Royals - Lorde"],
-		"correct_answer": 1
-	},
 	{
 		"audio": ["Song_Clips_Eng/closer.mp3", "Song_Clips_Eng/attention.mp3"],
 		"answers": ["Don't let me down - The Chainsmokers", "In the Name of Love - Martin Garrix", "Toothbrush - DNCE", "Closer - The Chainsmokers"],
 		"correct_answer": 4
 	},
 	{
-		"audio": ["Song_Clips_Eng/counting_stars.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Stressed Out - Twenty One Pilots", "Maps - Maroon 5", "Counting Stars - OneRepublic", "Hall of Fame - The Script"],
-		"correct_answer": 3
-	},
-	{
-		"audio": ["Song_Clips_Eng/dance_monkey.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Señorita - Shawn Mendes", "Don't Start Now - Dua Lipa", "Dance monkey - Tones and I", "Bad Guy - Billie Eilish"],
+		"audio": ["Song_Clips_Eng/believer.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Stressed out - Twenty One Pilots", "Mercy - One republic", "Believer - Imagine dragons", "If I lose Myself - One Republic"],
 		"correct_answer": 3
 	},
 	{
@@ -56,9 +16,14 @@ var eng_data =
 		"correct_answer": 1
 	},
 	{
-		"audio": ["Song_Clips_Eng/happier.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Scared to be Lonely - Martin Garrix", "The Middle - Zedd", "In The Name Of Love - martin Garrix", "Happier - Marshmello"],
+		"audio": ["Song_Clips_Eng/sweet_child_o_mine.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Highway to Hell - AC/DC", "Livin' On A Prayer - Bon Jovi", "Paradise City - Guns N' Roses", "Sweet Child O Mine - Guns N' Roses"],
 		"correct_answer": 4
+	},
+	{
+		"audio": ["Song_Clips_Eng/dance_monkey.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Senorita - Shawn Mendes", "Don't Start Now - Dua Lipa", "Dance monkey - Tones and I", "Bad Guy - Billie Eilish"],
+		"correct_answer": 3
 	},
 	{
 		"audio": ["Song_Clips_Eng/rockabye.mp3", "Song_Clips_Eng/attention.mp3"],
@@ -66,8 +31,28 @@ var eng_data =
 		"correct_answer": 2
 	},
 	{
-		"audio": ["Song_Clips_Eng/someone_you_loved.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Someone you loved - Lewis Capaldi", "Memories - Maroon 5", "Adore You - Harry Styles", "When the Party's Over - Billie Eilish"],
+		"audio": ["Song_Clips_Eng/another_one_bites_the_dust.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Under Pressure - Queen", "Smoke on the Water - Deep Purple", "Livin' On A Prayer - Bon Jovi", "Another one bites the dust - Queen"],
+		"correct_answer": 4
+	},
+	{
+		"audio": ["Song_Clips_Eng/back_to_you.mp3", "Song_Clips_Eng/attention.mp3", "Song_Clips_Eng/believer.mp3"],
+		"answers": ["Back to you - Salena Gomez", "Wolves - Salena Gomez", "Look what you made me do - Taylor swift", "Break free - Ariana Grande"],
+		"correct_answer": 1
+	},
+	{
+		"audio": ["Song_Clips_Eng/counting_stars.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Stressed Out - Twenty One Pilots", "Maps - Maroon 5", "Counting Stars - OneRepublic", "Hall of Fame - The Script"],
+		"correct_answer": 3
+	},
+	{
+		"audio": ["Song_Clips_Eng/another_brick_in_the_wall.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Break On Through - The Doors", "Another brick in the wall - Pink Floyd", "On an island - David Gilmour", "No Quarter - Led Zeppelin"],
+		"correct_answer": 2
+	},
+	{
+		"audio": ["Song_Clips_Eng/chandelier.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Chandelier - Sia", "Elastic Heart - Sia", "Blank Space - Taylor Swift", "Royals - Lorde"],
 		"correct_answer": 1
 	},
 	{
@@ -76,8 +61,23 @@ var eng_data =
 		"correct_answer": 2
 	},
 	{
-		"audio": ["Song_Clips_Eng/sweet_child_o_mine.mp3", "Song_Clips_Eng/attention.mp3"],
-		"answers": ["Highway to Hell - AC/DC", "Livin' On A Prayer - Bon Jovi", "Paradise City - Guns N' Roses", "Sweet Child O Mine - Guns N' Roses"],
+		"audio": ["Song_Clips_Eng/blinding_lights.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Yummy - Justin Beiber", "Blinding lights - The Weeknd", "Up all Night - Khalid", "Something In the Way You Move - Ellie Goulding"],
+		"correct_answer": 2
+	},
+	{
+		"audio": ["Song_Clips_Eng/attention.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Shake it off - Taylor Swift", "Attention - Charlie Puth", "New Rules - Dua Lipa", "Attention - Shawn Mendes"],
+		"correct_answer": 2
+	},
+	{
+		"audio": ["Song_Clips_Eng/happier.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Scared to be Lonely - Martin Garrix", "The Middle - Zedd", "In The Name Of Love - martin Garrix", "Happier - Marshmello"],
 		"correct_answer": 4
+	},
+	{
+		"audio": ["Song_Clips_Eng/someone_you_loved.mp3", "Song_Clips_Eng/attention.mp3"],
+		"answers": ["Someone you loved - Lewis Capaldi", "Memories - Maroon 5", "Adore You - Harry Styles", "When the Party's Over - Billie Eilish"],
+		"correct_answer": 1
 	}
 ]
